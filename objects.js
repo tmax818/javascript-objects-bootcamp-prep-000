b4270b457695f97fc78f0392b523d8d69420b675
Tyler Maxwell <tylermaxwell661@gmail.com>
2017-12-28 14:47:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,11 @@
-var playlist = {artistName: "songTitle"};
+var playlist = {artist: 'song'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName) {
+  var playlist = {}
  delete playlist.artistName;
  return artistName;
 }
