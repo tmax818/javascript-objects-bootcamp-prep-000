627aaa749791db612d30dd9438984bbcade5c70c
Tyler Maxwell <tylermaxwell661@gmail.com>
2017-12-28 14:25:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName) {
- delete playlist.artistName;
+ return delete playlist.artistName;
 }
